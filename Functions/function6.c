0e3810a3cb3b5d6c9d9feff780d95eb2fa60eb52
Gurchetan <gurchetan9966@gmail.com>
2024-08-12 19:10:26 +0530

    commit and push

--- Functions/function6.c
+++ Functions/function6.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
-
+// Function to calculate percentage from grades
 int percent(int phy, int chem, int math);
 
 int main(){
     printf("Enter your Grades Below\n");
 
     int phy;
-    printf("Physics:");
+    printf("Physics: ");
     scanf("%d", &phy);
 
     int chem;
-    printf("Chemistry:");
+    printf("Chemistry: ");
     scanf("%d", &chem);
 
     int math;
-    printf("Mathematics:");
+    printf("Mathematics: ");
     scanf("%d", &math);
 
-printf("Your Percentage is: %d\n", percent( phy, chem, math));
+    // Calculate and print the percentage
+    printf("Your Percentage is: %d\n", percent(phy, chem, math));
 }
+// Returns the average of the three grades
 int percent(int phy, int chem, int math){
-
-    return (( phy + chem + math)/3);
+    return ((phy + chem + math) / 3); // Integer division
 }
