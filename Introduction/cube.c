a55cdf4bc11769a15e4d0efd307254a6ddbc22b3
Gurchetan <gurchetan9966@gmail.com>
2024-09-12 22:23:15 +0530

    commit

--- Introduction/cube.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int num, cube;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-    cube = num * num * num;
-    printf("Cube of %d is %d\n", num, cube);
-    return 0;
-}
--- Introduction/square.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int num, square;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-    square = num * num;
-    printf("Square of %d is %d\n", num, square);
-    return 0;
-}
